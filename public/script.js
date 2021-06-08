14d30378f3e94785778fd1975cc2cec2e6900838
Karem Mohamed <karemkero21@gmail.com>
2021-06-08 21:49:17 +0200

    finish map functionality

--- public/script.js
+++ public/script.js
@@ -1,12 +1,33 @@
+const USER_MARKER =
+  'http://res.cloudinary.com/yemiwebby-com-ng/image/upload/v1526555652/user_my7yzc.png';
+const OFFLINE_MARKER =
+  'http://res.cloudinary.com/yemiwebby-com-ng/image/upload/v1526555651/offline_elrlvi.png';
+const ONLINE_MARKER =
+  'http://res.cloudinary.com/yemiwebby-com-ng/image/upload/v1526555651/online_bpf5ch.png';
+const RADIUS = 2000;
 new Vue({
   el: '#app',
   data: {
     users: [],
   },
+  created() {
+    let pusher = new Pusher('3ab930e3807d3e53660a', {
+      cluster: 'eu',
+    });
+
+    const channel = pusher.subscribe('map-geofencing');
+    channel.bind('location', (data) => {
+      this.initializeMap(data.person.position, data.people);
+    });
+  },
   mounted() {
     this.getUser();
   },
   methods: {
+    // load more users
+    loadMoreUsers() {
+      this.getUser();
+    },
     getUser() {
       axios.get('/users').then((response) => {
         this.users = this.getRandomUsers(response.data, 6);
@@ -24,6 +45,53 @@ new Vue({
         return users;
       });
       return selectedUsers;
+    },
+    getUserLocation(position) {
+      const user = { position };
+      axios.post('/users', user).then((response) => {
+        console.log(response);
+      });
+    },
+    initializeMap(position, people) {
+      const referencePoint = { lat: position.lat, lng: position.lng };
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        center: referencePoint,
+        zoom: 13,
+      });
+      for (var i = 0; i < people.length; i++) {
+        if (this.withinRegion(referencePoint, people[i], RADIUS)) {
+          this.addMarker(people[i], ONLINE_MARKER);
+        } else {
+          this.addMarker(people[i], OFFLINE_MARKER);
+        }
+      }
+      this.addCircle(position);
+    },
+    addMarker(props, marker) {
+      this.marker = new google.maps.Marker({
+        position: props.position,
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        icon: marker,
+      });
+    },
+    addCircle(position) {
+      this.circle = new google.maps.Circle({
+        map: this.map,
+        center: new google.maps.LatLng(position.lat, position.lng),
+        radius: 2000,
+        strokeColor: '#00ff00',
+        fillColor: '#484040bf',
+      });
+    },
+    withinRegion(position, user, radius) {
+      const to = new google.maps.LatLng(user.position.lat, user.position.lng);
+      const from = new google.maps.LatLng(position.lat, position.lng);
+      const distance = google.maps.geometry.spherical.computeDistanceBetween(
+        from,
+        to,
+      );
+      return distance <= radius;
     },
   },
 });
--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,7 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
 
 export class User {
-  @IsNotEmpty()
   name: string;
 
   @IsNotEmpty()
